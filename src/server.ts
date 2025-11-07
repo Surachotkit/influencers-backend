45f1e617a890abc4bd6c547a21c69c8ff8259f7f
Surachot Kittivijarn <surachot.kit@gmail.com>
2025-11-08 00:46:55 +0700

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+const PORT = process.env['PORT'] ?? 3000;
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello Node + Express + SQL!!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
